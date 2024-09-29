1cb09b49b4f681b724387aa21761663d022a2761
Brian Mak <brianmak.blue@gmail.com>
2024-09-29 15:35:26 -0700

    update code

--- modules/Utils.js
+++ modules/Utils.js
@@ -5,9 +5,17 @@ class Utils {
       return new Date().toString();
     }
   
-    static getGreeting(name) {
-      const greeting = require('../lang/en/en.json');
-      return `${greeting.message} ${name}. Server current date and time is ${this.getDate()}`;
+    // static getGreeting(name) {
+    //   const greeting = require('../lang/en/en.json');
+    //   return `${greeting.message} ${name}. Server current date and time is ${this.getDate()}`;
+    // }
+
+    static getGreetingMessage(name) {
+        const filePath = path.join(__dirname, '../lang/en/en.json');
+        const langFile = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+        const currentDate = this.getDate();
+        return langFile.greetingMessage.replace('%1', name).replace('%2', currentDate);
     }
   }
   
